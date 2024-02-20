3b0cf13b6afcf7fb8cbc703a87b4b0b762354343
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-20 18:47:43 -0300

    aprendiendo a usar react-router-dom, conociendo los términos de path, element, children y el componente Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Layout from './components/Layout';
+import NuevoCliente from './pages/NuevoCliente';
+import Index, { loader as indexLoader} from './pages/Index';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: indexLoader
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NuevoCliente />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
