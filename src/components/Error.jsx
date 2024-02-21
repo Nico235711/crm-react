b2c97209562adb3717a9e2d7d357a7e7d114fcde
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-21 16:27:55 -0300

    mostrando un mensaje de error

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+
+const Error = ({ children }) => {
+
+  return (
+    <p className="text-lg bg-red-700 uppercase text-white font-bold text-center py-1 my-3">{children}</p>
+  )
+}
+
+export default Error
