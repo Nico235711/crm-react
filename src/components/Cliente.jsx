799b00ca5a202800f792c648afae4948c55cb3e3
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-21 16:22:39 -0300

    usando Form para eliminar un cliente y redirect para redireccionar al usuario

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,7 +1,15 @@
+import { Form, useNavigate, redirect } from "react-router-dom";
+import { eliminarCliente } from "../data/clientes";
+
+export async function action({ params }) {
+  await eliminarCliente(params.clienteId)
+  return redirect("/")
+}
 
 const Cliente = ({ cliente }) => {
 
-  const { nombre, email, telefono, empresa } = cliente
+  const navigate = useNavigate()
+  const { nombre, email, telefono, empresa, id } = cliente
 
   return (
     <tr className="border-b">
@@ -15,12 +23,27 @@ const Cliente = ({ cliente }) => {
       </td>
       <td className="p-6">
         <div className="flex gap-5 justify-center">
-          <button className="bg-blue-700 px-3 py-1 text-white text-lg rounded hover:bg-blue-800 transition-all uppercase">
+          <button 
+            className="bg-blue-700 px-3 py-1 text-white text-lg rounded hover:bg-blue-800 transition-all uppercase" 
+            onClick={() => navigate(`/clientes/${id}/editar`)}
+          >
             Editar
           </button>
-          <button className="bg-red-700 px-3 py-1 text-white text-lg rounded hover:bg-red-800 transition-all uppercase">
-            Eliminar
-          </button>
+          <Form
+            method="post"
+            action={`clientes/${id}/eliminar`}
+            onSubmit={e => {
+              if (!confirm("¿Desea eliminar el cliente?")) {
+                e.preventDefault()
+              }
+            }}
+          >
+            <button 
+              className="bg-red-700 px-3 py-1 text-white text-lg rounded hover:bg-red-800 transition-all uppercase"
+            >
+              Eliminar
+            </button>
+          </Form>
         </div>
       </td>
     </tr>
